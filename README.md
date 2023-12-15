d06768b53225433fd4dffe987e63a8383910ef43
Carlos Sivira <carlos.csivira@gmail.com>
2023-12-14 23:59:55 -0400

    docs: readme and code comments

--- README.md
+++ README.md
@@ -1,9 +1,56 @@
 # Proyecto_2_LLP1_La_Leyenda_de_Celda
 
-# Ejecución
+## Ejecución
 
-```bash
+```prolog
 $ swipl
 ?- [main].
-?- cargar('data/mapa.txt', Mapa), cargar('data/palancas.txt', Palancas), cruzar(Mapa, Palancas, Seguro).
+?- leer(Mapa), data/mapa.txt.
+?- cruzar(Mapa, Palancas, Seguro).
+?- siempre_seguro(Mapa)
 ```
+
+## Implementación
+
+### Predicados auxiliares
+El proyecto se compone de los siguientes predicados auxiliares:
+
+* $concatenar/3$: Predicado para la concatenación de dos listas.
+
+* $eliminar\_elem/3$: Predicado que dado un elemento y una lista, obtiene una 
+nueva lista que elimina todas las existencias del elemento.
+
+* $eliminar/3$: Predicado que convierte una cadena de texto en una lista de 
+carateres, aplica el predicado $eliminar_char/3$ sobre cada elemento de la lista
+y vuelve a construir una nueva cadena de texto.
+
+* $cargar/2$: Utilidad para la carga del contenido de un archivo de texto como 
+un término.
+
+
+### Predicados principales
+ 
+* $cruzar/3$: Predicado principal que verifica si un Mapa y una configuración de
+Palancas es cruzable o no. Este predicado se descompone de la siguiente manera:
+    
+    * Verificación de pasillos: Verifica si un pasillo es _seguro_ o _trampa_. 
+    Compara el tipo de pasillo (_regular_, _de_cabeza_) con el modo de la 
+    palanca (_arriba_, _abajo_) asociado al pasillo con el mismo nombre.
+
+    * Verificación de juntas: Verifica si una junta es segura o trampa. Para 
+    esto, valida si la junta se compose de pasillos o de sub mapas. Si es un
+    pasillo entonces se valida usando la verificación de pasillos. Si es un
+    sub mapa (junta o bifurcación), se valida ya sea usando verificación de 
+    juntas o bifurcaciones según corresponda.
+
+    * Verificación de juntas: Caso análogo al anterior, Verifica si una
+    bifurcación es segura o trampa. 
+
+* $siempre\_seguro/2$: Verifica si un mapa es siempre cruzable 
+independientemente de la configuración de las palancas. Devuelve verdadero o
+falso dependiendo del valor obtenido por cruzar sin suministrar una Palanca
+específica.
+
+* $leer/1$: Solicita al usuario la ruta del archivo que contiene el mapa a 
+utilizar. Luego, obtiene el contenido del archivo en forma de término usando el
+predicado $cargar/2$.
--- src/main.pl
+++ src/main.pl
@@ -1,34 +1,69 @@
 /* 
-Universidad Simon Bolivar
-Departamento de computacion y Tecnologia de la Informacion
-CI3661 - Laboratorio de Lenguaje de Programacion
-Profesor: Ricardo Monascal
-Integrantes: 
-    - Carlos Sivira      15-11377
-    - Daniel Figueroa    16-10371
-    - Junior Lara        17-10303
-    - Astrid Alvarado    18-10938
-
-Proyecto - La Leyenda Celda y la Matriz Traspuesta
+* Universidad Simón Bolívar
+* Departamento de computación y Tecnología de la Información
+* CI3661 - Laboratorio de Lenguajes de Programación
+* Profesor: Ricardo Monascal
+* Integrantes: 
+*     - Carlos Sivira      15-11377
+*     - Daniel Figueroa    16-10371
+*     - Junior Lara        17-10303
+*     - Astrid Alvarado    18-10938
+* 
+* Proyecto 2 - La Leyenda Celda y la Matriz Traspuesta
 */
 
-% predicado para concatenar dos listas
+
+/******************************* Utilidades ***********************************/
+
+/* 
+* Predicado: concatenar/3
+* Argumentos: 
+*   - X: Lista base
+*   - Y: Lista a concatenar
+*   - Z: Lista resultante de concatenar X y Y
+* Comportamiento: Concatena dos listas
+*/
 concatenar([], Y, Y).
 concatenar([A|X], Y, [A|Z]) :- concatenar(X,Y,Z).
 
-% Utilidad para cargar archivos y transformar la entrada
-eliminar_char(_, [], []).
-eliminar_char(C, [C|T], R) :- eliminar_char(C, T, R).
-eliminar_char(C, [H|T], [H|R]) :- H \= C, eliminar_char(C, T, R).
 
+/* 
+* Predicado: eliminar_elem/3
+* Argumentos: 
+*   - C: Elemento a eliminar
+*   - L: Lista base
+*   - R: Lista resultante de eliminar todas las ocurrencias del elemento C
+* Comportamiento: Elimina las ocurrencias de un elemento en una lista
+*/
+eliminar_elem(_, [], []).
+eliminar_elem(C, [C|T], R) :- eliminar_elem(C, T, R).
+eliminar_elem(C, [H|T], [H|R]) :- H \= C, eliminar_elem(C, T, R).
+
+
+/* 
+* Predicado: eliminar/3
+* Argumentos: 
+*   - S: Cadena de texto base
+*   - C: Caracter a eliminar
+*   - R: Cadena de texto resultande de eliminar todas las ocurrencias de C.
+* Comportamiento: Elimina las ocurrencias de un caracter en una cadena de texto
+*/
 eliminar(S, C, R) :-
     string_chars(S, L),
-    eliminar_char(C, L, L2),
+    eliminar_elem(C, L, L2),
     string_chars(R, L2).
 
-cargar(File, Contenido):-
-    (exists_file(File) -> 
-        open(File, read, Stream),
+
+/* 
+* Predicado: cargar/2
+* Argumentos: 
+*   - Archivo: Path a un archivo de texto
+*   - Contenido: Contenido del archivo como término
+* Comportamiento: Lee un archivo texto y convierte su contenido en un término
+*/
+cargar(Archivo, Contenido):-
+    (exists_file(Archivo) -> 
+        open(Archivo, read, Stream),
         read_stream_to_codes(Stream, Code),
         close(Stream),
         atom_codes(Atom, Code),
@@ -38,27 +73,42 @@ cargar(File, Contenido):-
         term_string(Contenido, Atom4)
     ; write('Error: El archivo no existe.'), fail).
 
-/******************************* DEFINICION DE 'cruzar' ***********************************/
-% cruzar pasillos
+
+
+/******************************** cruzar/3 ************************************/
+
+/* 
+* Predicado: cruzar/3
+* Argumentos: 
+*   - Mapa: Representación de un laberinto como pasillos, bifucaciones y juntas
+*   - Palancas: Arreglo de pares (Letra, Modo)
+*   - Seguro: Verificación del estado del Mapa y Palancas en seguro o trampa
+* Comportamiento: Verifica si un Mapa con una configuración de Palancas se puede
+*                 cruzar. Retorna seguro si es cruzable, trampa si no 
+*/
+
+/* Verificación de pasillos */
 cruzar(pasillo(X, regular), [(X, arriba)], seguro).
 cruzar(pasillo(X, regular), [(X, abajo)], trampa).
 cruzar(pasillo(X, de_cabeza), [(X, arriba)], trampa).
 cruzar(pasillo(X, de_cabeza), [(X, abajo)], seguro).
 
-% cruzar pasillos que se juntan 
+/* Verificación de juntas */
 cruzar(junta(pasillo(X, Modo), pasillo(X, Modo)), Palancas, Seguro) :- 
     cruzar(pasillo(X, Modo), Palancas, Seguro), !.
-cruzar(junta(pasillo(X, Modo1), pasillo(X, Modo2)), _, seguro) :- Modo1 \= Modo2, !, fail.
+
+cruzar(junta(pasillo(X, Modo1), pasillo(X, Modo2)), _, seguro) :- 
+    Modo1 \= Modo2, !, fail.
+
 cruzar(junta(pasillo(X, Modo1), pasillo(X, Modo2)), Palancas, trampa) :- 
-    (
-        cruzar(pasillo(X, Modo1), Palancas, trampa)
-    ;
-        cruzar(pasillo(X, Modo2), Palancas, trampa),!
-    ).
+    cruzar(pasillo(X, Modo1), Palancas, trampa);
+    cruzar(pasillo(X, Modo2), Palancas, trampa), !.
+
 cruzar(junta(Submapa1, Submapa2), Palancas, seguro) :- 
     cruzar(Submapa1, P1, seguro),
     cruzar(Submapa2, P2, seguro),
     concatenar(P1,P2,Palancas).
+
 cruzar(junta(Submapa1, Submapa2), Palancas, trampa) :- 
     cruzar(Submapa1, P1, _),
     cruzar(Submapa2, P2, _),
@@ -66,20 +116,22 @@ cruzar(junta(Submapa1, Submapa2), Palancas, trampa) :-
     not(cruzar(junta(Submapa1, Submapa2), PosiblesPalancas, seguro)),
     PosiblesPalancas = Palancas.
 
-% cruzar bifurcaciones de pasillos
+/* Verificación de bifurcaciones */
 cruzar(bifurcacion(pasillo(X, Modo), pasillo(X, Modo)), Palancas, Seguro) :- 
     cruzar(pasillo(X, Modo), Palancas, Seguro), !.
-cruzar(bifurcacion(pasillo(X, Modo1), pasillo(X, Modo2)), _, seguro) :- Modo1 \= Modo2, !, fail.
+
+cruzar(bifurcacion(pasillo(X, Modo1), pasillo(X, Modo2)), _, seguro) :- 
+    Modo1 \= Modo2, !, fail.
+
 cruzar(bifurcacion(pasillo(X, Modo1), pasillo(X, Modo2)), Palancas, trampa) :- 
-    (
-        cruzar(pasillo(X, Modo1), Palancas, trampa)
-    ;
-        cruzar(pasillo(X, Modo2), Palancas, trampa),!
-    ).
+    cruzar(pasillo(X, Modo1), Palancas, trampa);
+    cruzar(pasillo(X, Modo2), Palancas, trampa), !.
+
 cruzar(bifurcacion(Submapa1, Submapa2), Palancas, trampa) :- 
     cruzar(Submapa1, P1, trampa),
     cruzar(Submapa2, P2, trampa),
     concatenar(P1,P2,Palancas).
+    
 cruzar(bifurcacion(Submapa1, Submapa2), Palancas, seguro) :- 
     cruzar(Submapa1, P1, _),
     cruzar(Submapa2, P2, _),
@@ -87,7 +139,16 @@ cruzar(bifurcacion(Submapa1, Submapa2), Palancas, seguro) :-
     not(cruzar(bifurcacion(Submapa1, Submapa2), PosiblesPalancas, trampa)),
     PosiblesPalancas = Palancas.
 
-/**************************** DEFINICION DE 'siempre_seguro' ********************************/
+
+
+/***************************** siempre_seguro/1 *******************************/
+
+/* 
+* Predicado: siempre_seguro/1
+* Argumentos: 
+*   - Mapa: Representación de un laberinto como pasillos, bifucaciones y juntas
+* Comportamiento: Verifica si un Mapa es siempre cruzable
+*/
 siempre_seguro(Mapa) :- 
     var(Mapa),
     (
@@ -98,7 +159,17 @@ siempre_seguro(Mapa) :-
     ), !.
 siempre_seguro(Mapa) :- cruzar(Mapa,_,seguro), !.
 
-/******************************** DEFINICION DE 'leer' *************************************/
+
+
+/********************************** leer/1 ************************************/
+
+/* 
+* Predicado: leer/1
+* Argumentos: 
+*   - Mapa: Representación de un laberinto como pasillos, bifucaciones y juntas
+* Comportamiento: Captura de la consola la ruta del archivo que contiene el mapa
+*                 a utilizar
+*/
 leer(Mapa) :-
     write('Ingrese en nombre del archivo: '),
     read(Archivo),
